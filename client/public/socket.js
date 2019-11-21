ce5a81426ebbe95fe3b46b60b119b0cb13461776
Daniel Cai <legoguy145@gmail.com>
2019-11-21 17:50:04 -0500

    added dormant code

--- client/public/socket.js
+++ client/public/socket.js
@@ -2,19 +2,24 @@ const socket = io();
 const suggestForm = document.getElementById('suggest-form');
 const suggestInput = document.getElementById('suggest-input');
 const skipForm = document.getElementById('skip-form');
+const threshold = 15;
 
-socket.on('track-skipped', data => {
-    if (data) {
-        // reset button state to false
-        setButtonState(false);
-    }
+socket.on('button-reset', (data) => {
+    // reset button state to false
+    setButtonState(false);
 });
 
-suggestForm.addEventListener('submit', e => {
+socket.on('current-track', (data) => {
     e.preventDefault();
     socket.emit('suggest-text', suggestInput.value);
     suggestInput.value = "";
 });
+
+// socket.on('vote-length', (data) => {
+//     let votesToGo = threshold - data;
+//     let string = `${votesToGo} votes left to skip`
+//     document.getElementById('votes-remaining').innerHTML = string;
+// });
 
 skipForm.addEventListener('click', e => {
     e.preventDefault();
